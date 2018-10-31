e2c3ddaeb83cfa0a97b591bb76c5159e3b9ac989
vdae2304 <vdae_2304@hotmail.com>
2018-10-30 23:00:36 -0600

    Update AVL Tree

--- Estructuras de datos/AVL Tree.cpp
+++ Estructuras de datos/AVL Tree.cpp
@@ -1,222 +1,196 @@
-#ifndef AVLTree_H_INCLUDED
-#define AVLTree_H_INCLUDED
+/*************************************************
+* Arbol AVL                                      *
+* Implementacion de un arbol AVL autobalanceable *
+*************************************************/
 
-template <class T> class AVLTree;
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
-template <class T>
-class AVLNode {
-    friend class AVLTree<T>;
+typedef char T; //Tipo de dato del arbol
 
-    private:
-        T item;                //Item a guardar
-        AVLNode<T> *p, *l, *r; //Padre, hijo izquierdo, hijo derecho
-        int h;                 //Altura
+struct node {
+    T item;      //Valor del nodo
+    int h, sz;   //Altura, Tamaño del subarbol
+    node *l, *r; //Hijos izquierdo y derecho
 
-    public:
-        /*Constructor*/
-        AVLNode(T item, AVLNode<T> *p = NULL, AVLNode<T> *l = NULL, AVLNode<T> *r = NULL, int h = 1) {
-            this->item = item;
-            this->p = p;
-            this->l = l;
-            this->r = r;
-            this->h = h;
-        }
-
-        /*Retorna el item*/
-        T getItem() const {
-            return item;
-        }
+    node(const T &x) {
+        item = x;
+        h = 0; sz = 1;
+        l = NULL; r = NULL;
+    }
 };
 
-template <class T>
-class AVLTree {
-    private:
-        AVLNode<T> *head; //Raiz
+/*Retorna la altura del arbol*/
+int height(node *root) {
+    if (root != NULL)
+        return root->h;
+    return 0;
+}
 
-        /*Retorna la altura del subarbol. 0 si es nulo*/
-        int height(AVLNode<T> *it) const {
-            return it != NULL ? it->h : 0;
+/*Retorna el tamaño del arbol*/
+int size(node *root) {
+    if (root != NULL)
+        return root->sz;
+    return 0;
+}
+
+/*Retorna el nodo con el elemento o NULL si no existe*/
+node *find(node *root, T x) {
+    if (root != NULL) {
+        if (x < root->item)
+            return find(root->l, x);
+        else if (root->item < x)
+            return find(root->r, x);
+    }
+    return root;
+}
+
+/*Retorna el k-esimo elemento del arbol*/
+node *kth_element(node *root, int k) {
+    if (root != NULL) {
+        if (k < size(root->l))
+            return kth_element(root->l, k);
+        else if (k > size(root->l))
+            return kth_element(root->r, k - size(root->l) - 1);
+    }
+    return root;
+}
+
+/*Retorna el indice del elemento en el arbol*/
+int index(node *root, const T &x) {
+    if (root != NULL) {
+        if (x < root->item)
+            return index(root->l, x);
+        else if (x == root->item)
+            return size(root->l);
+        else
+            return size(root->l) + 1 + index(root->r, x);
+    }
+    return 0;
+}
+
+/*Actualiza la altura y tamaño de un nodo*/
+void update(node *p) {
+    p->h = max(height(p->l), height(p->r)) + 1;
+    p->sz = size(p->l) + size(p->r) + 1;
+}
+
+/*Rota a la izquierda el nodo y retorna el nuevo padre*/
+node *left_rotate(node *x) {
+    node *y = x->r;
+    x->r = y->l;
+    update(x);
+    y->l = x;
+    update(y);
+    return y;
+}
+
+/*Rota a la derecha el nodo y retorna el nuevo padre*/
+node *right_rotate(node *y) {
+    node *x = y->l;
+    y->l = x->r;
+    update(y);
+    x->r = y;
+    update(x);
+    return x;
+}
+
+/*Calcula el factor de balance de un nodo*/
+int getBalance(node *p) {
+    if (p != NULL)
+        return height(p->l) - height(p->r);
+    return 0;
+}
+
+/*Balancea el subarbol de ser necesario*/
+node *Balance(node *p) {
+    if (getBalance(p) > 1) {
+        if (getBalance(p->l) >= 0)
+            return right_rotate(p);   //Caso izquierda-izquierda
+        else {
+            p->l = left_rotate(p->l); //Caso izquierda-derecha
+            return right_rotate(p);
         }
-
-        /*Rota a la izquierda el nodo it*/
-        void left_rotate(AVLNode<T> *it) {
-            AVLNode<T> *aux = it->r; //1) P           2) P
-                                     //   |              |
-            it->r = aux->l;          //   it             it    aux
-            if (aux->l != NULL)      //  /   \          /   \ /  \.
-                aux->l->p = it;      // Z   aux        Z     X    Y
-                                     //     /  \ .
-            aux->p = it->p;          //    X    Y
-            if (it->p == NULL)
-                head = aux;          //3)      P      4)   P
-            else if (it == it->p->l) //        |           |
-                it->p->l = aux;      //   it  aux         aux
-            else                     //  /  \ /  \        /  \.
-                it->p->r = aux;      // Z    X    Y     it    Y
-                                     //                /  \.
-            aux->l = it;             //               Z    X
-            it->p = aux;
-            it->h = std::max(height(it->l), height(it->r)) + 1;
-            aux->h = std::max(height(aux->l), height(aux->r)) + 1;
+    }
+    else if (getBalance(p) < -1) {
+        if (getBalance(p->r) <= 0)
+            return left_rotate(p);     //Caso derecha-derecha
+        else {
+            p->r = right_rotate(p->r); //Caso derecha-izquierda
+            return left_rotate(p);
         }
+    }
+}
 
-        /*Rota a la derecha el nodo it*/
-        void right_rotate(AVLNode<T> *it) {
-            AVLNode<T> *aux = it->l;  //1)   P        2)     P
-                                     //     |               |
-            it->l = aux->r;          //     it        aux   it
-            if (aux->r != NULL)      //    /  \       /  \ /  \.
-                aux->r->p = it;      //  aux   Y     Z    X    Y
-                                     //  /  \.
-            aux->p = it->p;          // Z    X
-            if (it->p == NULL)
-                head = aux;          //3) P          4)  P
-            else if (it == it->p->l) //   |              |
-                it->p->l = aux;      //  aux   it       aux
-            else                     //  /  \ /  \      /  \.
-                it->p->r = aux;      // Z    X    Y    Z   it
-                                     //                   /  \.
-            aux->r = it;             //                  X    Y
-            it->p = aux;
-            it->h = std::max(height(it->l), height(it->r)) + 1;
-            aux->h = std::max(height(aux->l), height(aux->r)) + 1;
-        }
+/*Inserta un nuevo elemento al arbol y lo balancea. Retorna la raiz*/
+node *insert(node *root, const T &x) {
+    if (root == NULL)
+        return new node(x);
+    else {
+        if (x < root->item)
+            root->l = insert(root->l, x);
+        else if (root->item < x) //No se permiten duplicados
+            root->r = insert(root->r, x);
+        update(root);
+        return Balance(root);
+    }
+}
 
-        /*Intercambia la posicion de dos subarboles dentro del arbol*/
-        void swap(AVLNode<T> *it1, AVLNode<T> *it2) {
-            if (it2 != NULL)
-                it2->p = it1->p;
-            if (it1->p == NULL)
-                head = it2;
-            else if (it1 == it1->p->l)
-                it1->p->l = it2;
-            else
-                it1->p->r = it2;
-        }
-
-        /*Calcula el factor de balance. Si el nodo es nulo, regresa 0*/
-        int getBalance(AVLNode<T> *it) {
-            return it != NULL ? height(it->l) - height(it->r) : 0;
-        }
-
-        /*Re-balancea el arbol desde un nodo dado*/
-        void Balance(AVLNode<T> *it) {
-            for (; it != NULL; it = it->p) {
-                it->h = std::max(height(it->l), height(it->r)) + 1;
-                if (getBalance(it) > 1) {
-                    if (getBalance(it->l) >= 0) //Caso izquierda-izquierda
-                        right_rotate(it);
-                    else {                      //Caso izquierda-derecha
-                        left_rotate(it->l);
-                        right_rotate(it);
-
-                    }
-                }
-                else if (getBalance(it) < -1) { //Caso derecha-derecha
-                    if (getBalance(it->r) <= 0)
-                        left_rotate(it);
-                    else {
-                        right_rotate(it->r);    //Caso derecha-izquierda
-                        left_rotate(it);
-                    }
-                }
-            }
-        }
-
-        /*Inserta un nuevo elemento al arbol y lo balancea*/
-        void AVLinsert(AVLNode<T> *&it, T item, AVLNode<T> *p) {
-            if (it == NULL) {
-                it = new AVLNode<T>(item, p);
-                Balance(it);
+/*Si el elemento esta en el arbol, lo elimina. Retorna la raiz*/
+node *erase(node *root, const T &x) {
+    if (root == NULL)
+        return root;
+    else {
+        if (x < root->item)
+            root->l = erase(root->l, x);
+        else if (root->item < x)
+            root->r = erase(root->r, x);
+        else {
+            if (root->l != NULL && root->r != NULL) {
+                root->item = kth_element(root->r, 0)->item;
+                root->r = erase(root->r, root->item);
             }
             else {
-                if (item < it->item)
-                    AVLinsert(it->l, item, it);
-                else
-                    AVLinsert(it->r, item, it);
+                node *aux = (root->l != NULL) ? root->l : root->r;
+                delete root;
+                return aux;
             }
         }
+        update(root);
+        Balance(root);
+        return root;
+    }
+}
 
-    public:
-        /*Constructor*/
-        AVLTree() {
-            head = NULL;
+/*Visualiza el arbol*/
+void Debug(node *root, int h = 0) {
+    if (root != NULL) {
+        Debug(root->l, h + 2);
+        for (int i = 0; i < h; i++)
+            cout << " ";
+        cout << root->item << "\n";
+        Debug(root->r, h + 2);
+    }
+}
+
+int main() {
+    node *arbol = NULL, *p;
+    int op = 1, k;
+    char c;
+    while (op != 0) {
+        cout << "1. Insertar letra\n2. Eliminar letra\n3. Buscar letra\n4. Letra en posicion\n5. Posicion de letra\n6. Imprimir arbol\n0. Salir\n";
+        cin >> op;
+        switch (op) {
+            case 1: cin >> c; arbol = insert(arbol, c); break;
+            case 2: cin >> c; arbol = erase(arbol, c); break;
+            case 3: cin >> c; cout << (find(arbol, c) != NULL ? "Esta en el arbol\n" : "No esta en el arbol\n"); break;
+            case 4: cin >> k; p = kth_element(arbol, k); cout << (p != NULL ? p->item : '*') << "\n"; break;
+            case 5: cin >> c; cout << index(arbol, c) << "\n"; break;
+            case 6: Debug(arbol);
         }
-
-        /*Retorna el apuntador a la raiz*/
-        AVLNode<T> *root() const {
-            return head;
-        }
-
-        /*Devuelve el apuntador al elemento minimo del subarbol*/
-        AVLNode<T> *min_element(AVLNode<T> *it) const {
-            if (it != NULL)
-                for (; it->l != NULL; it = it->l);
-            return it;
-        }
-
-        /*Devuelve el apuntador al elemento maximo del subarbol*/
-        AVLNode<T> *max_element(AVLNode<T> *it) const {
-            if (it != NULL)
-                for (; it->r != NULL; it = it->r);
-            return it;
-        }
-
-        /*Devuelve el apuntador al antecesor de it*/
-        AVLNode<T> *prev(AVLNode<T> *it) const {
-            if (it == NULL)
-                return NULL;
-            else if (it->l != NULL)
-                return max_element(it->l);
-            else {
-                for (; it->p != NULL && it == it->p->l; it = it->p);
-                return it->p;
-            }
-        }
-
-        /*Devuelve el apuntador al sucesor de it*/
-        AVLNode<T> *next(AVLNode<T> *it) const {
-            if (it == NULL)
-                return NULL;
-            else if (it->r != NULL)
-                return min_element(it->r);
-            else {
-                for (; it->p != NULL && it == it->p->r; it = it->p);
-                return it->p;
-            }
-        }
-
-        /*Devuelve el apuntador al nodo con el item buscado o NULL en caso contrario*/
-        AVLNode<T> *find(T item) const {
-            AVLNode<T> *it;
-            for (it = head; it != NULL && it->item != item; it = (item < it->item) ? it->l : it->r);
-            return it;
-        }
-
-        /*Si el elemento no esta en el arbol, lo inserta*/
-        void insert(T item) {
-            if (find(item) == NULL)
-                AVLinsert(head, item, NULL);
-        }
-
-        /*Si el elemento esta en el arbol, lo elimina*/
-        void erase(T item) {
-            AVLNode<T> *it = find(item);
-            if (it != NULL) {
-                //Si tiene dos hijos, intercambia posicion con el sucesor
-                if (it->l != NULL && it->r != NULL) {
-                    AVLNode<T> *aux = next(it);
-                    it->item = aux->item;
-                    it = aux;
-                }
-
-                //El nodo tiene a lo mas un hijo
-                AVLNode<T> *aux = (it->r == NULL) ? it->l : it->r;
-                swap(it, aux);
-                Balance(it);
-                delete it;
-            }
-        }
-};
-
-#endif // AVLTree_H_INCLUDED
+        system("pause");
+        system("cls");
+    }
+    return 0;
+}
