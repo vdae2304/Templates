11d3d523b94aff5eea6e3bf4059aed2e5e96eabe
vdae2304 <vdae_2304@hotmail.com>
2019-12-14 14:48:47 -0600

    Grafos

--- Grafos/Tarjan.cpp
+++ Grafos/Tarjan.cpp
@@ -14,38 +14,66 @@ using namespace std;
 int V, E;                //Numero de vertices y aristas.
 vector<int> graph[maxn]; //Aristas.
 
-int idx[maxn], low[ind], lst_id; //Indices, ultimo indice.
-stack<int> S;                    //Vertices pendientes.
-bool vis[maxn];                  //Visitado.
+vector<vector<int>> SCC; //Componentes fuertemente conexas.
+
+int idx[maxn], low[maxn], lst_id; //Indices, ultimo indice.
+stack<int> S;                     //Vertices pendientes.
+bool onStack[maxn];               //Esta en la pila.
 
 //Encuentra la componente fuertemente conexa de u.
 void StrongConnect(int u) {
     idx[u] = ++lst_id;
     low[u] = lst_id;
     S.push(u);
-    vis[u] = true;
+    onStack[u] = true;
 
     for (int v : graph[u]) {
         if(!idx[v]) {
             StrongConnect(v);
             low[u] = min(low[u], low[v]);
         }
-        else if (vis[v]) 
+        else if (onStack[v]) 
             low[u] = min(low[u], idx[v]);
     }
 
     if (low[u] == idx[u]) {
-        ;
+        SCC.push_back(vector<int>());
+        while (S.top() != u) {
+            onStack[S.top()] = false;
+            SCC.back().push_back(S.top());
+            S.pop();
+        }
+        onStack[u] = false;
+        SCC.back().push_back(u);
+        S.pop();
     }
 }
 
 //Algoritmo de Tarjan para encontrar las componentes fuertemente conexas.
 void Tarjan() {
-    for (int u = 0; u < V; ++v)
-        if (!vis[u])
+    for (int u = 0; u < V; ++u)
+        if (!idx[u])
             StrongConnect(u);
 }
 
 int main() {
+    ios_base::sync_with_stdio(0); cin.tie();
+    cin >> V >> E;
+
+    //Lee la informacion de las aristas.
+    for (int i = 0; i < E; ++i) {
+        int from, to;
+        cin >> from >> to;
+        graph[from].push_back(to);
+    }
+
+    //Imprime las componentes fuertemente conexas.
+    Tarjan();
+    for (int i = 0; i < SCC.size(); ++i) {
+        for (int u : SCC[i])
+            cout << u << ' ';
+        cout << '\n';
+    }
+
     return 0;
 }
