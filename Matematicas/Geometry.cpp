5d1e5d209ac45f3572e4ae31be0c8170f1053f1a
vdae2304 <vdae_2304@hotmail.com>
2020-01-03 16:41:42 -0600

    Matematicas

--- Matematicas/Geometry.cpp
+++ Matematicas/Geometry.cpp
@@ -76,11 +76,6 @@ point rotate_ccw(const point &P, double theta, bool radians = true) {
     return point(P.x*cos(theta) - P.y*sin(theta), P.x*sin(theta) + P.y*cos(theta));
 }
 
-//Regresa la proyeccion del vector P sobre el vector Q.
-point projection(const point &P, const point &Q) {
-    return Q * (dotProduct(P, Q) / dotProduct(Q, Q));
-}
-
 //Regresa el area del triangulo con vertices A, B y C.
 double areaTriangle(const point &A, const point &B, const point &C) {
     return crossProduct(B - A, C - A) / 2;
@@ -92,6 +87,34 @@ double areaPolygon(int n, const point P[]) {
     for (int i = 0; i < n - 1; ++i)
         area += P[i].x * P[i + 1].y - P[i + 1].x * P[i].y;
     return area / 2;
+}
+
+//Regresa la proyeccion del vector P sobre el vector Q.
+point projection(const point &P, const point &Q) {
+    return Q * (dotProduct(P, Q) / dotProduct(Q, Q));
+}
+
+//Regresa la distancia de un punto P a la recta que pasa por A y B.
+double distPointLine(const point &P, const point &A, const point &B) {
+    return dist(P, A + projection(P - A, B - A));
+}
+
+//Regresa true si el segmento CD corta a la recta que pasa por A y B, i.e., si los puntos
+//estan en lados opuestos de la recta.
+bool segmentLineIntersects(const point &A, const point &B, const point &C, const point &D) {
+    return crossProduct(B - A, C - A) * crossProduct(B - A, D - A) < 0;
+}
+
+//Regresa el punto de interseccion de dos rectas no paralelas que pasan por A, B y C, D.
+point lineLineIntersection(const point &A, const point &B, const point &C, const point &D) {
+    point v = B - A, w = D - C;
+    return A + v * (crossProduct(C - A, w) / crossProduct(v, w));
+}
+
+//Regresa el centro de la circunferencia que pasa por A, B y C.
+point circumcenter(const point &A, const point &B, const point &C) {
+    point MC = (A + B) / 2, MA = (B + C) / 2;
+    return lineLineIntersection(MC, MC + rotate90ccw(A - B), MA, MA + rotate90ccw(C - B));
 }
 
 int main() {
