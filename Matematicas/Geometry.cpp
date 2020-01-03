1075eb236040542b1d10f577616a4e73a3565f99
vdae2304 <vdae_2304@hotmail.com>
2020-01-03 15:19:48 -0600

    Matematicas

--- Matematicas/Geometry.cpp
+++ Matematicas/Geometry.cpp
@@ -6,15 +6,15 @@
 #include <cmath>
 using namespace std;
 
-typedef double T; //int o double
+#define PI 3.14159265358979323846 //20 primeros decimales.
 
 struct point {
-    T x, y;
+    double x, y;
 
     //Constructor.
-    point(T _x, T _y) {
-        x = _x;
-        y = _y;
+    point(double Px, double Py) {
+        x = Px;
+        y = Py;
     }
 
     //Regresa la suma de dos puntos.
@@ -28,42 +28,70 @@ struct point {
     }
 
     //Regresa el producto por un escalar.
-    point operator * (T lambda) {
+    point operator * (double lambda) {
         return point(x * lambda, y * lambda);
     }
 
     //Regresa el cociente entre un escalar.
-    point operator / (T lambda) {
+    point operator / (double lambda) {
         return point(x / lambda, y / lambda);
     }
 };
 
-//Regresa el producto punto.
-T dotProduct(point P, point Q) {
-    return P.x*Q.x + P.y*Q.y;
+//Regresa el producto punto de dos puntos.
+double dotProduct(const point &P, const point &Q) {
+    return P.x * Q.x + P.y * Q.y;
 }
 
-//Regresa la norma al cuadrado de un vector.
-T norm2(point P) {
-    return dotProduct(P, P);
+//Regresa la componente en z del producto cruz de dos puntos.
+double crossProduct(const point &P, const point &Q) {
+    return P.x * Q.y - P.y * Q.x;
 }
 
-//Regresa la distancia al cuadrado entre dos puntos.
-T dist2(point P, point Q) {
-    return norm2(P - Q);
+//Regresa la norma de un vector.
+double norm(const point &P) {
+    return sqrt(dotProduct(P, P));
 }
 
-//Regresa el angulo entre dos vectores.
-double angle(point P, point Q) {
-	return acos(dotProduct(P, Q) / sqrt(norm2(P) * norm2(Q)));
+//Regresa la distancia euclidiana entre dos puntos.
+double dist(const point &P, const point &Q) {
+    return norm(P - Q);
 }
 
-point CrossProduct(point P, point Q) {
-	return point(P.y*Q.z - P.z*Q.y, P.z*Q.x - P.x*Q.z, P.x*Q.y - P.y*Q.x);
+//Regresa el angulo en radianes entre dos vectores. Para verificar si son ortogonales 
+//(o paralelos), es mas rapido comprobar si su producto punto (resp. cruz) es cero.
+double angle(const point &P, const point &Q) {
+    return acos(dotProduct(P/norm(P), Q/norm(Q)));
 }
 
-point projection(point P, point Q) {
-	return (DotProduct(P, Q)/DotProduct(Q, Q)) * Q;
+//Regresa el vector rotado 90 grados en el sentido contrario de las manecillas del reloj.
+point rotate90ccw(const point &P) {
+    return point(-P.y, P.x);
+}
+
+//Regresa el vector rotado theta (radianes o grados) en el sentido contrario de las manecillas del reloj.
+point rotate_ccw(const point &P, double theta, bool radians = true) {
+    if (!radians)
+        theta *= PI/180;
+    return point(P.x*cos(theta) - P.y*sin(theta), P.x*sin(theta) + P.y*cos(theta));
+}
+
+//Regresa la proyeccion del vector P sobre el vector Q.
+point projection(const point &P, const point &Q) {
+	return Q * (dotProduct(P, Q) / dotProduct(Q, Q));
+}
+
+//Regresa el area del triangulo con vertices A, B y C.
+double areaTriangle(const point &A, const point &B, const point &C) {
+    return crossProduct(B - A, C - A) / 2;
+}
+
+//Regresa el area del poligono con vertices P[0], P[1], ... , P[n-1].
+double areaPolygon(int n, const point P[]) {
+    double area = P[n - 1].x * P[0].y - P[0].x * P[n - 1].y;
+    for (int i = 0; i < n - 1; ++i)
+        area += P[i].x * P[i + 1].y - P[i + 1].x * P[i].y;
+    return area / 2;
 }
 
 int main() {
