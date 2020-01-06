090daad6b98e1a44ac2512cba7c1cda5cdf2144e
vdae2304 <vdae_2304@hotmail.com>
2020-01-06 12:41:14 -0600

    Estructuras

--- Estructuras/PolicyBased.cpp
+++ Estructuras/PolicyBased.cpp
@@ -28,22 +28,18 @@ int main() {
     //Datos de ejemplo.
     int n = 10;
     int arr[] = {20, 15, 50, 30, 25, 36, 10, 35, 40, 21};
-
     //Crea un arbol con los datos de arr y lo imprime.
     ordered_set v, w;
     for (int i = 0; i < n; ++i)
         v.insert(arr[i]);
     print_tree(v.node_begin(), v.node_end()); cout << '\n';
-
     //Separa el arbol en dos y los imprime.
     v.split(30, w);
     print_tree(v.node_begin(), v.node_end()); cout << '\n';
     print_tree(w.node_begin(), w.node_end()); cout << '\n';
-
     //Vuelve a unir ambos arboles y lo imprime.
     v.join(w);
     print_tree(v.node_begin(), v.node_end()); cout << '\n';
-    
     //Imprime el indice de 35.
     cout << v.order_of_key(35) << '\n';
     //Imprime el 7-esimo elemento.
