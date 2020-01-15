b8a9ddf5dbe7074f0857aae04bb436dfc0af8058
vdae2304 <vdae_2304@hotmail.com>
2020-01-15 16:28:43 -0600

    Matematicas

--- Matematicas/Convex-Hull.cpp
+++ Matematicas/Convex-Hull.cpp
@@ -30,7 +30,7 @@ void GrahamScan() {
         if (P[i].y < P[first].y || (P[i].y == P[first].y && P[i].x < P[first].x))
             first = i;
     swap(P[0], P[first]);
-    sort(P, P + n, compareByAngle);
+    sort(P + 1, P + n, compareByAngle);
     hull.clear();
     for (int i = 0; i < n; ++i) {
         while (hull.size() > 1 && (conj(hull[hull.size()-1] - hull[hull.size()-2]) * 
