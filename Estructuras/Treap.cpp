702b496b121889b9d56bbee81a45b21a4e2ff49a
vdae2304 <vdae_2304@hotmail.com>
2020-05-16 15:44:56 -0500

    Estructuras

--- Estructuras/Treap.cpp
+++ Estructuras/Treap.cpp
@@ -4,7 +4,6 @@
 *********************************************************************************/
 
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 
 typedef int T;          //Tipo de las llaves.
@@ -43,19 +42,6 @@ void split(node *root, T key, node *&left, node *&right) {
     update(root);
 }
 
-//Inserta un nuevo elemento en un treap ordenado.
-void insert(node *&root, node *item) {
-    if (!root)
-        root = item;
-    else if (item->priority > root->priority) {
-        split(root, item->key, item->left, item->right);
-        root = item;
-    }
-    else
-        insert(item->key < root->key ? root->left : root->right, item);
-    update(root);
-}
-
 //Combina dos treaps en uno solo en el orden dado.
 void merge(node *&root, node *left, node *right) {
     if (!left || !right)
@@ -68,20 +54,6 @@ void merge(node *&root, node *left, node *right) {
         merge(right->left, left, right->left);
         root = right;
     }
-    update(root);
-}
-
-//Elimina una llave dada en un treap ordenado.
-void erase(node *&root, T key) {
-    if (!root)
-        return;
-    if (root->key == key) {
-        node *tmp = root;
-        merge(root, root->left, root->right);
-        delete tmp;
-    }
-    else 
-        erase(key < root->key ? root->left : root->right, key);
     update(root);
 }
 
@@ -102,9 +74,9 @@ void split_by_order(node *root, int k, node *&left, node *&right) {
 
 //Regesa el k-esimo elemento (indexado en 0) del treap.
 node *kth_element(node *root, int k) {
-    if (!root || k == size(root->left))
+    if (k == size(root->left))
         return root;
-    if (k < size(root->left))
+    else if (k < size(root->left))
         return kth_element(root->left, k);
     else 
         return kth_element(root->right, k - size(root->left) - 1);
@@ -114,56 +86,12 @@ node *kth_element(node *root, int k) {
 int order_of_key(node *root, T key) {
     if (key < root->key)
         return order_of_key(root->left, key);
-    if (key == root->key)
+    else if (key == root->key)
         return size(root->left) + 1;
     else
         return size(root->left) + 1 + order_of_key(root->right, key);
 }
 
-//Imprime los elementos de un treap.
-void print(node *root) {
-    if (root) {
-        print(root->left);
-        cout << root->key << ' ';
-        print(root->right);
-    }
-}
-
 int main() {
-    node *treap = NULL, *it, *l, *r;
-    char op;
-    T x;
-    int k;
-    
-    while (cin >> op) {
-        switch (op) {
-            case 'I': cin >> x; 
-                      insert(treap, new node(x, rand()));
-                      break;
-            case 'E': cin >> x; 
-                      erase(treap, x);
-                      break;
-            case 'K': cin >> k; 
-                      it = kth_element(treap, k); 
-                      if (it) 
-                        cout << it->key << '\n';
-                      break;
-            case 'O': cin >> x;
-                      cout << order_of_key(treap, x);
-                      break;
-            case 'S': cin >> x;
-                      split(treap, x, l, r);
-                      print(l); cout << '\n';
-                      merge(treap, l, r);
-                      break;
-            case 'F': cin >> k;
-                      split_by_order(treap, k, l, r);
-                      print(l); cout << '\n';
-                      merge(treap, l, r);
-                      break;
-            case 'P': print(treap); cout << '\n';
-        }        
-    }
-
     return 0;
 }
